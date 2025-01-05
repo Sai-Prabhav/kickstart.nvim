7ab3f50aa3f51258724f32ee1d3a01be591bb59d
Sai Prabhav <saiprabhav.ss@gmail.com>
2025-01-05 22:56:47 +0530

    o

---
+++ lua/snippets.lua
@@ -0,0 +1,10 @@
+local ls = require 'luasnip' -- Import LuaSnip
+local s = ls.snippet -- Define a snippet
+local t = ls.text_node -- Text node
+local i = ls.insert_node -- Insert node
+local conds = require 'luasnip.extras.conditions' -- Conditions
+
+-- Add the snippet for LaTeX files
+ls.add_snippets('tex', {
+  s('or', { t '\\cup' }, { condition = conds.in_mathzone }),
+})
